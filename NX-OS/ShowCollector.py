792ea81a4a16727e037efa4c166a046d043eb6a9
Diego S. Gancedo <dgancedo@gmail.com>
2018-08-31 13:12:57 -0300

    Added support list of switches using ',' as separator. Added support of using .txt file with switches per line as input.

--- NX-OS/ShowCollector.py
+++ NX-OS/ShowCollector.py
@@ -16,11 +16,11 @@ def main(argv):
    try:
       opts, args = getopt.getopt(argv,"hi:o:",["user=","password=","switch=","command="])
    except getopt.GetoptError:
-      print('ShowCollector.py --user <username> --password <ask|password> --switch <hostname|IP> --command <"command">')
+      print('ShowCollector.py --user <username> --password <ask|password> --switch <hostname|IP|list separated with ","|file.txt> --command <"command">')
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print('ShowCollector --user <username> --password <ask|password> --switch <hostname|IP> --command <"command">')
+         print('ShowCollector --user <username> --password <ask|password> --switch <hostname|IP|list separated with ","|file.txt> --command <"command">')
          sys.exit()
       elif opt in ("--user"):
          user = arg
@@ -37,12 +37,25 @@ def main(argv):
 
 if __name__ == "__main__":
    values=main(sys.argv[1:])
-   net_connect = ConnectHandler(device_type='cisco_nxos', ip=values[2], username=values[0], password=values[1]) 
-   switchname = net_connect.send_command("show hostname")
-   print("Executing "+ values[3] + " on " + switchname)
-   show = net_connect.send_command(values[3])
-   filename = switchname.replace(" ","_") + values[3].replace(" ", "_") + ".txt"
-   f = open(filename,'w')
-   f.write(show)
-   f.close
-   print("File " + filename + "generated with the output")
+   if values[0] != "" and values[1] != "" and values[2] != "" and values[3] !="":
+      switches = []
+      if ".txt" in values[2]:
+         s = open(values[2],'r')
+         for line in s:
+            switches.append(line.replace("\n",""))
+      elif "," in values[2]:
+         switches = values[2].split(",")
+      else:
+         switches.append(values[2])
+      for switch in switches:
+         net_connect = ConnectHandler(device_type='cisco_nxos', ip=switch, username=values[0], password=values[1]) 
+         switchname = net_connect.send_command("show hostname")
+         print("Executing "+ values[3] + " on " + switchname)
+         show = net_connect.send_command(values[3])
+         filename = switchname.replace(" ","_") + values[3].replace(" ", "_") + ".txt"
+         f = open(filename,'w')
+         f.write(show)
+         f.close
+         print("File " + filename + " generated with the output")
+   else:
+      main('-h')  
