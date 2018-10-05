a955fd46c89269a5a37d7ccca48083f658fdc86e
Diego S. Gancedo <dgancedo@Narsil.local>
2018-10-05 15:31:01 -0300

    Added an option to connect to an nxos or ios

--- NX-OS/ShowCollector.py
+++ NX-OS/ShowCollector.py
@@ -12,18 +12,27 @@ def main(argv):
    user = ''
    password = ''
    switch = ''
+   devtype = ''
    command = ''
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["user=","password=","switch=","command="])
+      opts, args = getopt.getopt(argv,"hi:o:",["user=","password=","switch=","type=","command="])
    except getopt.GetoptError:
-      print('ShowCollector.py --user <username> --password <ask|password> --switch <hostname|IP|list separated with ","|file.txt> --command <"command">')
+      print('ShowCollector.py --user <username> --password <ask|password> --switch <hostname|IP|list separated with ","|file.txt> --type [nxos|ios] --command <"command">')
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print('ShowCollector --user <username> --password <ask|password> --switch <hostname|IP|list separated with ","|file.txt> --command <"command">')
+         print('ShowCollector --user <username> --password <ask|password> --switch <hostname|IP|list separated with ","|file.txt> --type [nxos|ios] --command <"command">')
          sys.exit()
       elif opt in ("--user"):
          user = arg
+      elif opt in ("--type"):
+         if arg == 'nxos':
+            devtype = 'cisco_nxos'
+         elif arg == 'ios':
+            devtype = 'cisco_ios'
+         else:
+            print ('Invalid device type')
+            sys.exit()
       elif opt in ("--password"):
          if arg == 'ask':
             password = getpass.getpass()
@@ -33,11 +42,11 @@ def main(argv):
          switch = arg
       elif opt in ("--command"):
          command = arg
-   return user,password,switch,command
+   return user,password,switch,devtype,command
 
 if __name__ == "__main__":
    values=main(sys.argv[1:])
-   if values[0] != "" and values[1] != "" and values[2] != "" and values[3] !="":
+   if values[0] != "" and values[1] != "" and values[2] != "" and values[3] !="" and values[4] !="":
       switches = []
       if ".txt" in values[2]:
          s = open(values[2],'r')
@@ -48,14 +57,14 @@ if __name__ == "__main__":
       else:
          switches.append(values[2])
       for switch in switches:
-         net_connect = ConnectHandler(device_type='cisco_nxos', ip=switch, username=values[0], password=values[1]) 
+         net_connect = ConnectHandler(device_type=values[3], ip=switch, username=values[0], password=values[1]) 
          switchname = net_connect.send_command("show hostname")
-         print("Executing "+ values[3] + " on " + switchname)
-         show = net_connect.send_command(values[3])
+         print("Executing "+ values[4] + " on " + switchname)
+         show = net_connect.send_command(values[4])
          filename = switchname.replace(" ","_") + values[3].replace(" ", "_") + ".txt"
          f = open(filename,'w')
          f.write(show)
          f.close
          print("File " + filename + " generated with the output")
    else:
-      main('-h')  
+      print('ShowCollector.py --user <username> --password <ask|password> --switch <hostname|IP|list separated with ","|file.txt> --type [nxos|ios] --command <"command">')  
